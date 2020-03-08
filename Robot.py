379263b48b7bfb511ca2ee930252d1de25eaec9a
DCtime <daniel.chu916@gmail.com>
2020-03-08 22:16:21 +0800

    Robot class: add guess()

--- Robot.py
+++ Robot.py
@@ -1,10 +1,25 @@
 from Entity import Entity
-
+import random
 
 
 class Robot(Entity):
     def __init__(self, name):
         self.__name = name
+        self.__ans = None
 
     def guess(self):
-        pass
+        """ return a random num with correct format """
+        while 1:
+            self.__ans = random.randint(1000, 9999)
+            self.__ans = str(self.__ans)
+            if len(set(self.__ans)) == 4:
+                if self.__ans[0] != 0:
+                    break
+        return self.__ans
+
+    # getter and setter
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
