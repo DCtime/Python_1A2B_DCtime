bff7fe2f9d3d3e1633f2ad15ff3caa8087387897
DCtime <daniel.chu916@gmail.com>
2020-04-21 09:15:42 +0800

    add player window 完成

--- Test_GUI.py
+++ Test_GUI.py
@@ -2,12 +2,15 @@ import tkinter as tk
 
 
 def open_welcome_window():
+    def close_welcome_window():
+        welcome_window.destroy()
+        open_add_player_window()
+
     # FOR DEBUG: we are now opening welcome window
     print("Open : Opening Welcome window")
 
     """ Welcome window """
     # the start of the welcome window
-    global welcome_window
     welcome_window = tk.Tk()
     welcome_window.title("Welcome")
     welcome_window.geometry("400x200")
@@ -52,21 +55,23 @@ def open_welcome_window():
     welcome_window.mainloop()
 
 
-def close_welcome_window():
-    welcome_window.destroy()
-    open_add_player_window()
-
-
 def open_add_player_window():
+    def close_add_player_window():
+        player_quantity = entry_player_quantity.get()
+        robot_quantity = entry_robot_quantity.get()
+        print("player quantity = ", player_quantity)
+        print("robot quantity = ", robot_quantity)
+        add_player_window.destroy()
+        open_enter_name_window()
+
     # FOR DEBUG : we are now opening add player window
     print("Open : Opening Add player window")
 
     """ add_player_window """
-    global add_player_window
     add_player_window = tk.Tk()
     add_player_window.title("Add players")
     add_player_window.geometry("400x200")
-    add_player_window.resizable(0, 0)
+    # add_player_window.resizable(0, 0)
 
     # widgets in add_player_window
 
@@ -93,25 +98,64 @@ def open_add_player_window():
         font="Helvetica 15 bold",
         relief='groove'
     )
+    # entry_robot_quantity : Entry object
+    # enter robot's quantity
+    entry_robot_quantity = tk.Entry(
+        font="Helvetica 15"
+    )
+
+    button_enter = tk.Button(
+        add_player_window,
+        text="Enter",
+        font="Helvetica 15",
+        command=close_add_player_window
+    )
+
+    label_blank = tk.Label(
+        add_player_window
+    )
 
     # packing widgets
-    label_player_quantity.pack(
-        anchor='nw',
-        side='left'
+    label_player_quantity.grid(
+        row=0, column=0,
     )
 
-    entry_player_quantity.pack(
-        anchor='nw',
-        side='left'
+    entry_player_quantity.grid(
+        row=0, column=1, columnspan=2
     )
 
-    label_robot_quantity.pack(
-        
+    label_robot_quantity.grid(
+        row=1, column=0,
+    )
+
+    entry_robot_quantity.grid(
+        row=1, column=1, columnspan=2
+    )
+
+    label_blank.grid(
+        row=2, column=0, columnspan=2
+    )
+    button_enter.grid(
+        row=3, column=1, sticky='w'
     )
 
     # keep the window
     add_player_window.mainloop()
 
 
+def open_enter_name_window():
+    def close_enter_name_window():
+        pass
+
+    # FOR DEBUG: we are now opening enter name window
+    print("Open : Opening Enter name window")
+
+    """ Enter name window """
+    # the start of the enter name window
+    enter_name_window = tk.Tk()
+    enter_name_window.title("Enter names")
+    enter_name_window.geometry("1000x1000")
+
+    
 """ >>> Showing the windows <<< """
 open_welcome_window()
